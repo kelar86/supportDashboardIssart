afc02b434a092abc1a3e05f6e72a9e8ef698b643
volynshchikov <volynshchikov@aigeo.ru>
2019-03-21 17:42:59 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MessageTableComponent } from './components/message-table/message-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MessageTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/message-table/message-table.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-table',
+  template: `
+    <p>
+      message-table works!
+    </p>
+  `,
+  styles: []
+})
+export class MessageTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
